f628bf8040da49be5c5acbac0b8bcb2a2f526185
ben awad <benawad97@gmail.com>
2017-03-04 21:48:57 -0600

    all

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const game = require('./game');
+const authentication = require('./authentication');
+const user = require('./user');
+
+module.exports = function() {
+  const app = this;
+
+
+  app.configure(authentication);
+  app.configure(user);
+  app.configure(game);
+};
